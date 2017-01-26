019ad17d368b35378206c79e9f82ef2f8860441a
albanninou <albanninou@gmail.com>
2017-01-26 14:29:58 +0100

    mise  jour graphique

--- java/fr/albanninou/frogone/MainActivity.java
+++ java/fr/albanninou/frogone/MainActivity.java
@@ -1,12 +1,20 @@
 package fr.albanninou.frogone;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     static LinearLayout menu;
@@ -14,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     static int lvl = 0;
     static Context context;
     static Activity activity;
+    static MainActivity ac;
 
     public static void setButton() {
         lvl = preferences.getInt("lvl", 0);
@@ -32,6 +41,59 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = preferences.edit();
         context = this;
         activity = this;
+        ac = this;
         setButton();
+        Update();
     }
+
+    public void Update() {
+        //get destination to update file and set Uri
+        //TODO: First I wanted to store my update .apk file on internal storage for my app but apparently android does not allow you to open and install
+        //aplication with existing package from there. So for me, alternative solution is Download directory in external storage. If there is better
+        //solution, please inform us in comment
+        String destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/";
+        String fileName = "FrogOne.apk";
+        destination += fileName;
+        final Uri uri = Uri.parse("file://" + destination);
+
+        //Delete update file if exists
+        File file = new File(destination);
+        if (file.exists())
+            //file.delete() - test this, I think sometimes it doesnt work
+            file.delete();
+
+        //get url of app on server
+        String url = "http://frogone.esy.es/FrogOne.apk";
+
+        //set downloadmanager
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+
+        //set destination
+        request.setDestinationUri(uri);
+
+        // get download service and enqueue file
+        final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        final long downloadId = manager.enqueue(request);
+
+        //set BroadcastReceiver to install app when .apk is downloaded
+        BroadcastReceiver onComplete = new BroadcastReceiver() {
+            public void onReceive(Context ctxt, Intent intent) {
+                Intent install = new Intent(Intent.ACTION_VIEW);
+                install.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                install.setDataAndType(uri,
+                        manager.getMimeTypeForDownloadedFile(downloadId));
+                startActivity(install);
+                Intent promptInstall = new Intent(Intent.ACTION_VIEW)
+                        .setDataAndType(uri,
+                                "application/vnd.android.package-archive");
+                startActivity(promptInstall);
+                unregisterReceiver(this);
+                finish();
+            }
+        };
+        //register receiver for when .apk download is compete
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
 }
